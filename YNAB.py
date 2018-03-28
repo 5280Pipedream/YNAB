b166c3bc10b5febe55f35a2d3130f0a53193298a
root <root@SUPPORT-7510.localdomain>
2018-03-28 16:16:29 -0600

    initial commit

---
+++ YNAB.py
@@ -0,0 +1,10 @@
+# Capitol One csv format to YNAB csv format for import into YNAB
+
+import os
+import pandas as pd 
+import datetime
+
+location = r'D:\dev\YNAB\schwab.csv'
+schwab = pd.read_csv(location)
+Account = 'Investor Checking'
+YNAB = ['Account', 'Date', 'Payee', 'Outflow', 'Inflow']
