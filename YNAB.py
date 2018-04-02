1b060a31adb7b61e6754b5cea487e5eaf9c67c61
Duane Rodarte <duane7997@gmail.com>
2018-04-02 10:41:17 -0600

    updated

--- YNAB.py
+++ YNAB.py
@@ -1,10 +1,22 @@
+#!/usr/bin/env python3
 # format to YNAB csv format (headers) for import into YNAB
 
 import os
 import pandas as pd 
 import datetime
 
-location = r'D:\dev\YNAB\schwab.csv'
+location = r'E:\Projects\YNAB\schwab.csv'
 schwab = pd.read_csv(location)
-Account = 'Investor Checking'
 YNAB = ['Account', 'Date', 'Payee', 'Outflow', 'Inflow']
+
+schwab = pd.read_csv(location, names = ['Date', 'Type', 'Check #', 'Payee', 'Outflow', 'Inflow', 'Running Balance'])
+ # remove columns
+del schwab['Type']
+del schwab['Check #']
+del schwab['Running Balance']
+# add account column
+schwab['Account'] = 'Investor Checking'
+# order columns to match YNAB
+schwab[['Account', 'Date', 'Payee', 'Outflow', 'Inflow']] # sort by columns
+# export to CSV
+schwab.to_csv(r'E:\Projects\YNAB\schwab_ReadyToExport.csv')
